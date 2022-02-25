42b7721faae1b270183386f432881a6ad58739b2
Batdorj <batdorj.ds@gmail.com>
2022-02-25 19:40:35 +0800

    implemented auth

--- api/index.js
+++ api/index.js
@@ -13,6 +13,10 @@ mongoose.connect(process.env.MONGO_URL, {
 .then(()=> console.log("DB Connection Successfull!"))
 .catch((err) => console.log(err));
 
+
+
+app.use(express.json());
+
 app.use("/api/auth", authRoute);
 
 
